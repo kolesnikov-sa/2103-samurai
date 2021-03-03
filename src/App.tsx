31fed64a796236c7519ff3915e5728246bd312e8
kolesnikov-sa <kolesnikov-sa@ya.ru>
2021-03-03 15:36:31 +0300

    END Lesson 10 corrected file name footer.tsx -> Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Technologies} from "./Technologies";
 import {Header} from "./Header";
-import { Footer } from './footer';
+import { Footer } from './Footer';
 
 function App() {
     return (
