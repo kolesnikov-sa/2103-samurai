47f30b5c307350068a22d7dcc53749747ba5fcdd
sergeykolesnikov <kolesnikov-sa@ya.ru>
2021-03-12 16:08:23 +0300

    END Lesson 22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Dialogs.module.css';
 
 function Dialogs() {
     return (
       <div className={s.dialog}>
         <div className={s.dialog__items}>
-          <div className={s.dialog__item + ' ' + s.dialog__item_active}>Kolya</div>
-          <div className={s.dialog__item}>Petya</div>
-          <div className={s.dialog__item}>Masha</div>
-          <div className={s.dialog__item}>Varya</div>
-          <div className={s.dialog__item}>Irina</div>
+          <div className={s.dialog__item + ' ' + s.dialog__item_active}><NavLink to={'/dialogs/1'}>Kolya</NavLink></div>
+          <div className={s.dialog__item}><NavLink to={'/dialogs/2'}>Petya</NavLink></div>
+          <div className={s.dialog__item}><NavLink to={'/dialogs/3'}>Masha</NavLink></div>
+          <div className={s.dialog__item}><NavLink to={'/dialogs/4'}>Varya</NavLink></div>
+          <div className={s.dialog__item}><NavLink to={'/dialogs/5'}>Irina</NavLink></div>
+          <div className={s.dialog__item}><NavLink to={'/dialogs/6'}>Tikhon</NavLink></div>
         </div>
         <div className={s.dialog__messages}>
           <div className={s.dialog__message}>Message 1</div>
