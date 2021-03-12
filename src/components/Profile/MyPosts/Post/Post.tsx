1ba733583ccc943bfce0198b8eee17302a80bd5f
sergeykolesnikov <kolesnikov-sa@ya.ru>
2021-03-12 13:38:02 +0300

    END Lesson 18 corrected

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -4,7 +4,7 @@ import s from './Post.module.css';
 function Post(props: any) {
     return (
         <div className={s.post}>
-            <img className={s.post__avatar} src={"https://www.nicepng.com/png/detail/186-1866063_dicks-out-for-harambe-sample-avatar.png"} />
+            <img className={s.post__avatar} src={"https://www.nicepng.com/png/detail/186-1866063_dicks-out-for-harambe-sample-avatar.png"} alt={'img-alt'}/>
             <div className={s.post__content}>{props.message}</div>
             <div className={s.post__like}>Likes: {props.likesCount}</div>
         </div>
