5f940873554f5de4b2d0ea1a342d9f410983df48
sergeykolesnikov <kolesnikov-sa@ya.ru>
2021-03-12 14:14:58 +0300

    END Lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className={'app-wrapper'}>
-      <Header/>
-      <Navbar/>
-      <div className={'app-wrapper__content'}>
-        {/*<Profile/>*/}
-        <Dialogs/>
+    <BrowserRouter>
+      <div className={'app-wrapper'}>
+        <Header/>
+        <Navbar/>
+        <div className={'app-wrapper__content'}>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/dialogs' component={Dialogs}/>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import s from './Navbar.module.css';
 function Navbar() {
     return (
         <nav className={s.nav}>
-            <a href={"#"} className={`${s.nav__item} ${s.nav__item_active}`}>Profile</a>
-            <a href={"#"} className={s.nav__item}>Messages</a>
+            <a href={"/profile"} className={`${s.nav__item} ${s.nav__item_active}`}>Profile</a>
+            <a href={"/dialogs"} className={s.nav__item}>Messages</a>
             <a href={"#"} className={s.nav__item}>News</a>
             <a href={"#"} className={s.nav__item}>Music</a>
             <a href={"#"} className={s.nav__item}>Settings</a>
