3791634252bea8fe819b3449b73abe008ae2dc56
sergeykolesnikov <kolesnikov-sa@ya.ru>
2021-03-12 15:45:22 +0300

    END Lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,21 @@ import s from './Dialogs.module.css';
 
 function Dialogs() {
     return (
-      <div>
-        Dialogs
+      <div className={s.dialog}>
+        <div className={s.dialog__items}>
+          <div className={s.dialog__item + ' ' + s.dialog__item_active}>Kolya</div>
+          <div className={s.dialog__item}>Petya</div>
+          <div className={s.dialog__item}>Masha</div>
+          <div className={s.dialog__item}>Varya</div>
+          <div className={s.dialog__item}>Irina</div>
+        </div>
+        <div className={s.dialog__messages}>
+          <div className={s.dialog__message}>Message 1</div>
+          <div className={s.dialog__message}>Message 2</div>
+          <div className={s.dialog__message}>Message 3</div>
+          <div className={s.dialog__message}>Message 4</div>
+          <div className={s.dialog__message}>Message 5</div>
+        </div>
       </div>
     );
 }
